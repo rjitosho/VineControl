f10b31bca0462356319995eb9d7fa6263cd7a86b
Rianna Jitosho <rjitosho@stanford.edu>
2021-01-31 23:57:01 -0800

    update RBD acrobot reference

--- rbd/rbdDoublePen.jl
+++ rbd/rbdDoublePen.jl
@@ -1,19 +1,23 @@
+using Pkg; Pkg.activate("rbd")
 using RigidBodyDynamics
 using LinearAlgebra
 using StaticArrays
 using Plots
+using MeshCat
+using MeshCatMechanisms
 
 g = -9.81 # gravitational acceleration in z-direction
 world = RigidBody{Float64}("world")
 doublependulum = Mechanism(world; gravity = SVector(0, 0, g))
 
 axis = SVector(0., 1., 0.) # joint axis
-I_1 = 0.333 # moment of inertia about joint axis
+I_1 = 1/3 # moment of inertia about joint axis
+I_1 = .292
 c_1 = -0.5 # center of mass location with respect to joint axis
 m_1 = 1. # mass
 frame1 = CartesianFrame3D("upper_link") # the reference frame in which the spatial inertia will be expressed
 inertia1 = SpatialInertia(frame1,
-    moment=I_1 * axis * axis',
+    moment=Matrix(Diagonal([I_1,I_1,.5*m_1*.1^2])),
     com=SVector(0, 0, c_1),
     mass=m_1)
 
@@ -24,13 +28,13 @@ before_shoulder_to_world = one(Transform3D,
 attach!(doublependulum, world, upperlink, shoulder,
     joint_pose = before_shoulder_to_world)
 
-
 l_1 = -1. # length of the upper link
-I_2 = 0.333 # moment of inertia about joint axis
+I_2 = 1/3 # moment of inertia about joint axis
+I_2 = .292
 c_2 = -0.5 # center of mass location with respect to joint axis
 m_2 = 1. # mass
 inertia2 = SpatialInertia(CartesianFrame3D("lower_link"),
-    moment=I_2 * axis * axis',
+    moment=Matrix(Diagonal([I_2,I_2,.5*m_2*.1^2])),
     com=SVector(0, 0, c_2),
     mass=m_2)
 lowerlink = RigidBody(inertia2)
@@ -40,14 +44,23 @@ before_elbow_to_after_shoulder = Transform3D(
 attach!(doublependulum, upperlink, lowerlink, elbow,
     joint_pose = before_elbow_to_after_shoulder)
 
+# mvis = MechanismVisualizer(doublependulum)
+# set_configuration!(mvis, [0.0, 0.0])
+# open(mvis)
+
 state = MechanismState(doublependulum)
 set_configuration!(state, shoulder, 0.3)
 set_configuration!(state, elbow, 0.4)
-set_velocity!(state, shoulder, 1.)
-set_velocity!(state, elbow, 2.);
+set_velocity!(state, shoulder, 0.)
+set_velocity!(state, elbow, 0.);
 
 setdirty!(state)
-ts, qs, vs = simulate(state, 2., Δt = 1e-3);
+ts, qs, vs = simulate(state, 1., Δt = 1e-3);
 
-plot([qs[i][1] for i=1:2000])
-plot([qs[i][2] for i=1:2000])
+# animation = MeshCatMechanisms.Animation(mvis, ts, qs)
+# setanimation!(mvis, animation)
+
+pyplot()
+N = length(qs)
+plot([qs[i][1] for i=1:N])
+plot!([qs[i][2] for i=1:N])
